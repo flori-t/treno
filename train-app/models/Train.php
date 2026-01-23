89efc4f54879e12b75cc0af71417fff8f8de6046
Aula <you@example.com>
2026-01-23 14:10:29 +0100

    Train Class

---
+++ train-app/models/Train.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/Wagon.php';
+class Train extends Wagon {
+
+    public $wagon;
+
+    public function __construct($passengers = 0, $wagon=[]){
+        parent::__construct($passengers);
+        $this->wagon = $wagon;
+    }
+    public function add_wagon($wagon){
+        $wagon = new Wagon;
+        array_push($this->wagon, $wagon);
+        
+    }
+    public function passengers_distribution(){
+
+    }
+
+    
+}
+
+$train = new Train;
+$train-> add_wagon($wagon);
+$train-> add_wagon($wagon2);
+$train-> add_wagon($wagon3);
+var_dump($train);
